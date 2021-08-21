3ff091ef37f097ca491933f2f6845d48fb034761
Charlie <45357714+GoodPro712@users.noreply.github.com>
2021-08-22 05:21:47 +1000

    Bunch of stuff + gradle wrapper

--- src/main/java/net/telluriummc/telluriumchatbridge/TelluriumDiscordBot.java
+++ src/main/java/net/telluriummc/telluriumchatbridge/TelluriumDiscordBot.java
@@ -2,13 +2,18 @@ package net.telluriummc.telluriumchatbridge;
 
 import club.minnced.discord.webhook.WebhookClient;
 import club.minnced.discord.webhook.send.WebhookMessageBuilder;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.filter.TextStream;
+import net.minecraft.server.command.CommandManager;
 import net.minecraft.util.Formatting;
 import net.telluriummc.telluriumchatbridge.utils.JsonReader;
 import org.apache.commons.codec.binary.Base64;
@@ -18,16 +23,18 @@ import org.mineskin.MineskinClient;
 import org.mineskin.SkinOptions;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class TelluriumDiscordBot
 {
     private JDA bot;
     private List<TextChannel> bridgeChannels;
     private final MinecraftServer mcServer;
-    private static final int DISCORD_MAX_CHARACTER_LIMIT = 2000;
 
     public TelluriumDiscordBot(String token, MinecraftServer server, String[] channelIds) {
         try {
@@ -56,55 +63,135 @@ public class TelluriumDiscordBot
             // loop through every channel id and add the channels to the bridgeChannels list
             for (String channelId : channelIds) {
                 TextChannel channel = bot.getTextChannelById(channelId);
-                String name = channel != null ? channel.getName() : "channel does not exist";
+
+                if (channel == null) {
+                    System.out.println("[Tellurium Chat Bridge] Discord channel initialization failed due to channel not existing. Channel id provided: " + channelId);
+                    continue;
+                }
+
                 // add the channel and print this info to console
                 bridgeChannels.add(channel);
-                System.out.println("[Tellurium Chat Bridge] Initialized Discord channel with the name of " + name  + " and the id of " + channel.getId());
+                System.out.println("[Tellurium Chat Bridge] Initialized Discord channel with the name of " + channel.getName()  + " and the id of " + channelId);
             }
         }).start();
     }
 
     public void handleDiscordMessage(MessageReceivedEvent event) {
-        // Handles all messages sent in any channel that the bot has permissions for, in any server.
-        String messageContent = event.getMessage().getContentRaw();
-        String botPrefix = "!"; // todo: config
+        // todo: config
+        String filler = " " + Formatting.RESET + Formatting.DARK_GRAY + "»" + Formatting.RESET + " ";
+        String nameSuffix = " " + Formatting.BLUE + Formatting.BOLD + "D";
+        String botPrefix = "!";
 
-        // todo: commands
+        // temp cache variables for less method calls
+        Message message = event.getMessage();
+        MessageChannel channel = event.getChannel();
+        User author = event.getAuthor();
+        String messageContent = message.getContentRaw();
+        String eventChannelId = channel.getId();
+
         if (messageContent.startsWith(botPrefix)) {
             switch (messageContent.substring(botPrefix.length())) {
-                case "ping":
-                    event.getChannel().sendMessage("Pong!").queue();
-                    break;
+                case "ping" -> channel.sendMessage("Pong! Latency: `" + bot.getGatewayPing() + "ms`").queue();
+                case "online", "players", "playing" -> {
+                    StringBuilder playerList = new StringBuilder();
+                    for (String player : mcServer.getPlayerNames()) {
+                        playerList.append(player).append(", ");
+                    }
+
+                    // remove the ", " from the final string
+                    String formattedPlayerList = playerList.toString().equals("") ? "No players online" : playerList.substring(0, playerList.length() - 2);
+
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setTitle("**Online Players (" + mcServer.getCurrentPlayerCount() + ")**");
+                    embed.addField("Players", formattedPlayerList, false);
+                    channel.sendMessageEmbeds(embed.build()).queue();
+                }
+                case "health", "stats", "tps", "mspt" -> {
+                    float mspt = mcServer.getTickTime() + 50; // millisecond sleep
+                    float tps = 1000 / (mspt); // 1000 because millisecond to second
+
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setTitle("**Server Health**");
+                    embed.addField("TPS", "Server TPS - " + tps + "/20", true);
+                    embed.addField("MSPT", "Server MSPT - " + mspt, true);
+                    channel.sendMessageEmbeds(embed.build()).queue();
+                }
             }
+        }
+
+        // make sure we don't send anything if the content is sent by our bot and starts with ```[
+        boolean channelToChannelContent = author.getId().equals(bot.getSelfUser().getId()) && messageContent.startsWith("**[");
+        if (message.isWebhookMessage() || channelToChannelContent) {
+            return;
         }
 
         // loop through every text channel
         for (TextChannel bridgeChannel : bridgeChannels) {
+            // check if the channel the message was sent doesn't match our text channel, and make sure it isn't a webhook
+            if (!eventChannelId.equals(bridgeChannel.getId())) {
+                continue;
+            }
 
-            // check if the channel the message was sent doesn't match our text channel
-            if (event.getChannel().getId().equals(bridgeChannel.getId())) {
-                String messagePrefix = "[Discord]"; // todo: config
+            // send messages to every other channel
+            handleDiscordToDiscord(eventChannelId, event);
+        }
 
-                // Checks if the message isn't sent by the bot, or if it is, it only runs if the message doesn't start with [Discord]
-                if (!event.getMessage().getAuthor().isBot() || !(messageContent.startsWith(messagePrefix) && event.getAuthor() == bot.getSelfUser())) {
-                    // Handling discord messages to minecraft | todo: config
-                    String filler = " " + Formatting.RESET + Formatting.DARK_GRAY + "»" + Formatting.RESET + " ";
-                    String nameSuffix = " " + Formatting.BLUE + Formatting.BOLD + "D";
+        // Sends a message that looks something like GoodPro712 D » This is a message from Discord!
+        if (mcServer.getCurrentPlayerCount() > 0) {
+            CommandManager commandManager = mcServer.getCommandManager();
+            String attachment = !message.getAttachments().isEmpty() ? "<Attachment> " : "";
+            commandManager.execute(mcServer.getCommandSource(), formatAsTellraw(author.getName() + nameSuffix + filler + attachment + messageContent));
+        }
+    }
 
-                    // Sends a message that looks something like GoodPro712 D » This is a message from Discord!
-                    mcServer.getCommandManager().execute(mcServer.getCommandSource(), formatAsTellraw(event.getAuthor().getName() + nameSuffix + filler + messageContent));
+    private void handleDiscordToDiscord(String eventChannelId, MessageReceivedEvent event) {
+        for (TextChannel bridgeChannel : bridgeChannels) {
+            if (bridgeChannel.getId().equals(eventChannelId)) {
+                continue;
+            }
+
+            String authorName = event.getAuthor().getName();
+            String messageGuild = event.getGuild().getName();
+            String header = "**[" + authorName + " from " + messageGuild + "]**\n";
+            Message message = event.getMessage();
+            List<Message.Attachment> attachments = message.getAttachments();
+
+            MessageAction newMessage = bridgeChannel.sendMessage(header + message.getContentRaw());
+
+            if (!attachments.isEmpty()) {
+                for (Message.Attachment attachment : attachments) {
+                    try {
+                        newMessage.addFile(attachment.downloadToFile().get());
+                    } catch (InterruptedException | ExecutionException error) {
+                        error.printStackTrace();
+                    }
                 }
             }
+
+            newMessage.queue();
         }
     }
 
     public void handleMinecraftMessage(String rawMessage, String uuid) {
-        // create webhook builder todo: webhook url from config
+        // create webhook builder | todo: webhook url from config
         WebhookClient client = WebhookClient.withUrl("https://discord.com/api/webhooks/877690625481392179/FQO9otfxTI6Ml7mMY6dt-5IpacF4KhYnvz-EgmcwfFm7G1_PNg8cvwAFOjD4QaVC_cdY");
         WebhookMessageBuilder builder = new WebhookMessageBuilder();
 
         // add the minecraft message to the webhook builder
         builder.append(rawMessage);
+
+        if (uuid.equals("00000000000000000000000000000000")) {
+            builder.setUsername("[SERVER]");
+
+            try {
+                builder.setAvatarUrl(new File(String.valueOf(mcServer.getIconFile())).toURI().toURL().toString());
+            } catch (MalformedURLException error) {
+                error.printStackTrace();
+            }
+
+            client.send(builder.build());
+            return;
+        }
 
         // set the webhook username to minecraft IGN
         try {
@@ -138,7 +225,7 @@ public class TelluriumDiscordBot
         }
     }
 
-    private static String formatAsTellraw(String message) {
+    private static @NotNull String formatAsTellraw(String message) {
         // remove multiline
         message = message.replaceAll("\n", " ");
         // log message
@@ -151,7 +238,6 @@ public class TelluriumDiscordBot
                     "}";
     }
 
-    // todo: handle errors
     public static String usernameFromUUID(String uuid) throws IOException {
         JSONObject usernameData = JsonReader.fromUrl("https://api.mojang.com/user/profile/" + uuid);
         return usernameData.getString("name");
---
+++ src/main/java/net/telluriummc/telluriumchatbridge/mixin/MinecraftServerMixin.java
@@ -0,0 +1,21 @@
+package net.telluriummc.telluriumchatbridge.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.text.Text;
+import net.telluriummc.telluriumchatbridge.TelluriumChatBridge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+    @Inject(method = "sendSystemMessage", at = @At("HEAD"))
+    public void sendMessage(Text text, UUID uuid, CallbackInfo ci) {
+        String parsedUuid = uuid.toString().replaceAll("-", "");
+        String rawMessage = text.asString();
+        TelluriumChatBridge.discordBot.handleMinecraftMessage(rawMessage, parsedUuid);
+    }
+}
